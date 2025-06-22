69ab0ebf6ee7d203f6e1bb57ae3f870be4899827
Ubuntu <ubuntu@ip-172-31-6-207.ap-northeast-1.compute.internal>
2025-06-22 13:37:18 +0000

    updating productpage

--- productpage/version.tf
+++ productpage/version.tf
@@ -8,6 +8,9 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path = "/home/ubuntu/.kube/config"
-
+  host                   = var.k8s_host
+  client_certificate     = var.k8s_client_certificate
+  client_key             = var.k8s_client_key
+  cluster_ca_certificate = var.k8s_cluster_ca_cert
 }
+
