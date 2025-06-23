3f15a41007f239495001162ba45cfdf9bffd1025
naynwezinmyint <54239589+naynwezinmyint@users.noreply.github.com>
2025-06-24 01:28:02 +0900

    Update version.tf

--- version.tf
+++ version.tf
@@ -8,5 +8,13 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path = "/home/ubuntu/.kube/config"
+  host                   = var.k8s_host
+  cluster_ca_certificate = base64decode(var.k8s_ca_cert)
+  client_certificate     = base64decode(var.k8s_client_cert)
+  client_key             = base64decode(var.k8s_client_key)
 }
+
+variable "k8s_host" {}
+variable "k8s_ca_cert" {}
+variable "k8s_client_cert" {}
+variable "k8s_client_key" {}
