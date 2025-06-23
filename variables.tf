5a628fd2b884facaacb1a5b01800535f181aff64
nnzm-test <naynwezinmyint1995@gmail.com>
2025-06-24 00:32:50 +0900

    upload files

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "service_account_name" {
+  description = "Name of the Kubernetes ServiceAccount"
+  type        = string
+}
+
+variable "container_name" {
+  description = "Name of the container, used for labels and selectors"
+  type        = string
+}
+
+variable "service_name" {
+  description = "Name of the Kubernetes Service"
+  type        = string
+}
+
+variable "deployment_name" {
+  description = "Name of the Kubernetes Deployment"
+  type        = string
+}
+
+variable "image" {
+  description = "Docker image for the container"
+  type        = string
+}
+
+variable "port" {
+  description = "Port the application exposes and Prometheus scrapes"
+  type        = number
+}
